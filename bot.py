60c351ab1daa7f8aeb39f8bdc8b0b2ca674d3989
Alex Korotkov <durm.icecoffee@gmail.com>
2016-07-22 02:16:57 +0300

    inline

--- bot.py
+++ bot.py
@@ -1,36 +1,48 @@
 #!/usr/bin/env python
 import sys
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from telegram.keyboardbutton import KeyboardButton
-from telegram import InlineKeyboardButton, CallbackQuery, InlineKeyboardMarkup
-from telegram.replykeyboardmarkup import ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, InlineQueryHandler, Filters
+from telegram import InlineQueryResultArticle, InputTextMessageContent, ParseMode
 import logging
 from lxml import etree
 import re
+import uuid
 
-# Enable logging
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
-
 NT_URL = "http://127.0.0.1:8080/exist/rest/db/nz/nz.xml"
 
+PATTERN_SINGLE = "<Книга>. <Глава>:<Стих>"
+PATTERN_RANGE = "<Книга>. <Глава>:<Стих>-<Стих>"
 
-# Define a few command handlers. These usually take the two arguments bot and
-# update. Error handlers also receive the raised TelegramError object in error.
+INDEX = re.compile("^(?P<book>\w+)\.\s(?P<chapter>\d+):(?P<verse_from>\d+)(\-(?P<verse_to>\d+))?")
+HELP = """
+Синтаксис запросов:
+*{}* для конкретного стиха
+или
+*{}* для диапазона стихов
+(максимальное количество стихов в диапазоне равно 10)
+Например: _Мф. 5:3-12_
+""".format(PATTERN_SINGLE, PATTERN_RANGE)
+
 def start(bot, update):
-    bot.sendMessage(update.message.chat_id, text='Здравствуй! Это навигатор по Библии!')
+    WELCOME = """
+Здравствуй! Это навигатор по Библии!\n
+Вы можете получить выдержки из Нового Завета по запросам.\n
+{}
+    """.format(HELP)
+    bot.sendMessage(update.message.chat_id, text=WELCOME, parse_mode=ParseMode.MARKDOWN)
 
 
 def help(bot, update):
-    bot.sendMessage(update.message.chat_id, text='Help!')
+    bot.sendMessage(update.message.chat_id, text=HELP, parse_mode=ParseMode.MARKDOWN)
 
 
 def build_selector(query):
-    expr = re.compile("^\s*(?P<book>\w+)\.\s*(?P<chapter>\d+)\s*:\s*(?P<verse_from>\d+)\s*(\-\s*(?P<verse_to>\d+)\s*)?")
-    result = re.search(expr, query)
+    result = re.search(INDEX, query)
     if result:
         resultdict = result.groupdict()
         return """//book[short-title="{book}"]/chapter[{chapter}]/verse[position()={verse_from}{to_part}]""".format(
@@ -42,14 +54,35 @@ def get_url(myselector):
     return NT_URL + '?_query=' + myselector
 
 
+def get_text_by_selector(myselector):
+    url = NT_URL + '?_query=' + myselector
+    result = etree.parse(url)
+    message = "\n".join(result.xpath("//verse/text()"))
+    return message[0:4000] + "..." if len(message) > 4000 else message
+
+
+def inlinequery(bot, update):
+    query = update.inline_query.query
+    if re.match(INDEX, query):
+        myselector = build_selector(query)
+        message = get_text_by_selector(myselector)
+        message += "\n_" + " ".join(query.split()) + "_"
+        results = list()
+        results.append(
+            InlineQueryResultArticle(id=uuid.uuid4(),
+                                     title=query,
+                                     input_message_content=InputTextMessageContent(
+                                         message, parse_mode=ParseMode.MARKDOWN),
+                                     parse_mode=ParseMode.MARKDOWN)
+        )
+        bot.answerInlineQuery(update.inline_query.id, results=results)
+
+
 def show(bot, update):
     query = update.message.text
     myselector = build_selector(query)
     if myselector:
-        url = NT_URL + '?_query=' + myselector
-        result = etree.parse(url)
-        message = "\n".join(result.xpath("//verse/text()"))
-        message = message[0:4000] + "..." if len(message) > 4000 else message
+        message = get_text_by_selector(myselector)
         if message:
             bot.sendMessage(update.message.chat_id, text=message)
         else:
@@ -57,8 +90,10 @@ def show(bot, update):
     else:
         bot.sendMessage(update.message.chat_id, text="Проверьте правильность сокращений!")
 
+
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
+
 
 def main(token):
     # Create the EventHandler and pass it your bot's token.
@@ -71,8 +106,8 @@ def main(token):
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
 
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler([Filters.text], show))
+    #dp.add_handler(MessageHandler([Filters.text], show))
+    dp.add_handler(InlineQueryHandler(inlinequery))
 
     # log all errors
     dp.add_error_handler(error)
